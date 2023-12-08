848568af4ae7e1fe342d586cab475dc1e37f134b
git-pixel22 <workemail.rajat@gmail.com>
2023-12-09 02:24:30 +0530

    not-working

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 chrome.commands.onCommand.addListener(function(command) {
-    if (command === "open-chatgpt") {
-      chrome.tabs.executeScript({
-        code: `
-          const selectedText = window.getSelection().toString().trim();
-          const encodedQuery = encodeURIComponent(selectedText);
-          const url = 'https://chat.openai.com?query=' + encodedQuery;
-          window.open(url, '_blank');
-        `
-      });
-    }
-  });
-  
+  if (command === "open-chatgpt") {
+    chrome.tabs.executeScript({
+      code: `
+        const selectedText = window.getSelection().toString().trim();
+        const encodedQuery = encodeURIComponent(selectedText);
+        const url = 'https://chat.openai.com?query=' + encodedQuery;
+        const newTab = window.open(url, '_blank');
+
+        newTab.onload = function() {
+          newTab.document.getElementById('prompt-textarea').value = selectedText;
+          newTab.document.getElementById('prompt-textarea').focus();
+        };
+      `
+    });
+  }
+});
