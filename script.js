c17866d7925c0206258c384731c6281b28f4c91a
git-pixel22 <workemail.rajat@gmail.com>
2023-12-11 13:32:18 +0530

    commit

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-chrome.commands.onCommand.addListener(function(command) {
+chrome.commands.onCommand.addListener(function (command) {
   if (command === "open-chatgpt") {
     chrome.tabs.executeScript({
       code: `
         const selectedText = window.getSelection().toString().trim();
-        const encodedQuery = encodeURIComponent(selectedText);
-        const url = 'https://chat.openai.com?query=' + encodedQuery;
+        const url = 'https://chat.openai.com/';
+
         const newTab = window.open(url, '_blank');
 
         newTab.onload = function() {
-          newTab.document.getElementById('prompt-textarea').value = selectedText;
-          newTab.document.getElementById('prompt-textarea').focus();
+          const elem = newTab.document.querySelector("textarea");
+          elem.value = selectedText;
+          elem.focus();
         };
       `
     });
